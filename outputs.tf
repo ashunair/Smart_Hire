828c2fab51289ebe62148f2fa9a2860038e51098
ashunair <ashwathynair1998@gmail.com>
2024-07-23 20:42:13 +0000

    terraform output file

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "dev_cluster_name" {
   value       = google_container_cluster.dev_cluster.name
 }
 
-output "stage_cluster_name" {
-  description = "The name of the stage cluster"
-  value       = google_container_cluster.stage_cluster.name
-}
-
 output "prod_cluster_name" {
   description = "The name of the prod cluster"
   value       = google_container_cluster.prod_cluster.name
